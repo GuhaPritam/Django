85f934d90d9c72d8e7c6f6d03484547e12413cea
Pritam Guha <pritam@tenxerlabs.com>
2025-03-01 15:54:09 +0530

    added css

--- my_tennis_club/my_tennis_club/views.py
+++ my_tennis_club/my_tennis_club/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hellow, HOME page")
+    return render(request, 'websites/index.html')
 
 def about(request):
     return HttpResponse("Hellow, ABOUT page")
