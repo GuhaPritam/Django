68e4f339afb0989daffce4e25effe00135fe45be
Pritam Guha <pritam@tenxerlabs.com>
2025-03-01 15:20:44 +0530

    added templates

--- my_tennis_club/my_tennis_club/views.py
+++ my_tennis_club/my_tennis_club/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
 def home(request):
-    
+    return HttpResponse("Hellow, HOME page")
+
+def about(request):
+    return HttpResponse("Hellow, ABOUT page")
+
+def contact(request):
+    return HttpResponse("Hellow, CONTACT page")
