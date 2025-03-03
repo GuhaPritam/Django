f43212d1e9c325a561c56dd5eda591c567132ef8
Pritam Guha <pritam@tenxerlabs.com>
2025-03-03 10:36:21 +0530

    added enother app

--- my_tennis_club/app/apps.py
+++ my_tennis_club/app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PracticeAppConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'practice_app'
+    name = 'app'
