5b03080422386eb4bf7db7a958913beda93c5b28
Pritam Guha <pritam@tenxerlabs.com>
2025-03-05 21:17:34 +0530

    reloaded the css

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'app',
     'tailwind',
     'theme',
+    'django_browser_reload',
 ]
 
 TAILWIND_APP_NAME = "theme"
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'my_tennis_club.urls'
