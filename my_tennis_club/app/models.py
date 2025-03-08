5af9a4351ca6818f56ca124602b0e56e0437dc12
Pritam Guha <pritam@tenxerlabs.com>
2025-03-08 15:21:26 +0530

    added models for db

--- my_tennis_club/app/models.py
+++ my_tennis_club/app/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 class AppVarity(models.Model):
+    APP_TYPE_CHOICE = [
+        ('FD', 'FOOD'),
+        ('PL', 'PLAY'),
+        ('SG', 'SING'),
+    ]
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=APP_TYPE_CHOICE)
